1439ace8d1d7089eb0f8227451dcd21e8bda9468
BTajini <badr.tajini@edu.dsti.institute>
2017-02-17 21:02:19 +0100

    Initial commit

---
+++ SparkStreamingTwitter-master/src/main/scala/com/bkomar/app/TwitterStreamingCollector.scala
@@ -0,0 +1,64 @@
+package com.bkomar.app
+
+import com.bkomar.utils.Utils
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.twitter.TwitterUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+object TwitterStreamingCollector {
+  private var numTweetsCollected = 0L //count for the tweet collected
+  private var partNum = 0
+  private var numTweetsToCollect = 10 // num of tweet to collect
+
+  def main(args: Array[String]) {
+    //number of args except filters
+    val baseParamsCount = 3
+    if (args.length < 4) {
+      System.err.println("Run streaming with the following parameters: <outputPath> <batchIntervalSeconds> " +
+        "<partitionsNum> <filtering keywords>")
+      System.exit(1)
+    }
+
+    //TODO bk move to utils
+    val outputPath: String = args(0)
+    val batchInterval: Int = args(1).toInt
+    val partitionNum: Int = args(2).toInt
+    val keyWordsFilters: Seq[String] = args.takeRight(args.length - baseParamsCount)
+
+    println("Collector is executed with the filters: " + keyWordsFilters.mkString(Utils.hashTagSeparator))
+
+    val sparkConf = new SparkConf().setAppName("TwitterStreamingCollector")
+    val ssc = new StreamingContext(sparkConf, Seconds(batchInterval))
+
+    Utils.setUpTwitterOAuth
+
+    val twitterStream = TwitterUtils.createStream(ssc, None, keyWordsFilters)
+    twitterStream.foreachRDD((rdd, time) => {
+      if (rdd.count() > 0) {
+        println("Numbmer of tweets received: " + rdd.count())
+        rdd
+          .map(t => (
+            t.getUser.getName,
+            t.getCreatedAt.toString,
+            t.getText,
+            t.getHashtagEntities.map(_.getText).mkString(Utils.hashTagSeparator)
+            //            t.getRetweetCount  issue in twitter api, always returns 0
+          ))
+        numTweetsCollected += count
+          .repartition(partitionNum)
+          //.coalesce(1, shuffle = true).saveAsTextFile(outputPath + "tweets" + time.milliseconds.toString + ".txt")
+          .coalesce(1, shuffle = true).saveAsTextFile(outputPath + "tweets" + ".txt")
+        if (numTweetsCollected > numTweetsToCollect) {
+          System.exit(0) // exit from the streaming after have collected 10 tweets !
+        }
+
+
+        //TODO bk add checkpointing
+        //test for hive and sql context
+      }
+    })
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
---
+++ SparkStreamingTwitter-master/src/main/scala/com/bkomar/utils/Utils.scala
@@ -0,0 +1,50 @@
+package com.bkomar.utils
+
+import java.util.Properties
+import scala.io.Source
+
+
+object Utils {
+
+  def hashTagSeparator = "|"
+
+  /**
+   * Set up Twitter oAuth connection properties
+   */
+  def setUpTwitterOAuth() = {
+    val props = readProperties("app.properties")
+    System.setProperty("twitter4j.oauth.consumerKey",
+      props.getProperty("twitter.oauth.consumer.key"))
+    System.setProperty("twitter4j.oauth.consumerSecret",
+      props.getProperty("twitter.oauth.consumer.secret"))
+    System.setProperty("twitter4j.oauth.accessToken",
+      props.getProperty("twitter.oauth.access.token"))
+    System.setProperty("twitter4j.oauth.accessTokenSecret",
+      props.getProperty("twitter.oauth.access.token.secret"))
+
+//    System.setProperty("twitter4j.http.connectionTimeout", "50000")
+//    System.setProperty("twitter4j.http.readTimeout", "220000")
+  }
+
+  /**
+   * Loads properties from app classpath
+   * @param fileName properties file name
+   * @return properties
+   */
+  def readProperties(fileName: String) : Properties = {
+    val url = getClass.getResource("/".concat(fileName))
+    val properties = new Properties()
+    if (url != null) {
+      try {
+        val reader = Source.fromURL(url).bufferedReader()
+        properties.load(reader)
+        reader.close()
+      } catch {
+        case ex: Exception =>
+          System.err.println("Application properties can not be loaded." + ex.getMessage)
+          System.exit(1)
+      }
+    }
+    return properties
+  }
+}
